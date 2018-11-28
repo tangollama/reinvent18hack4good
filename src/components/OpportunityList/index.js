cc02951885a0591a9926604d5e226285f351d119
Joel Worrall <jworrall@newrelic.com>
2018-11-27 17:13:25 -0800

    filtering in progress

--- src/components/OpportunityList/index.js
+++ src/components/OpportunityList/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { renderToString } from 'react-dom/server'
+import TableFilter from 'react-table-filter'
 
 export default class OpportunityList extends Component {
 
@@ -9,43 +11,77 @@ export default class OpportunityList extends Component {
     }
   
     constructor(props) {
-      super(props)
+        super(props)
+        this.state = {
+            data: null
+        }
+    }
+
+    _filterUpdated(newData){
+		this.setState({
+			data: newData
+		});
+    }
+    
+    _loadData() {
+        if (this.state.data == null) {
+            const data = this.props.opportunities.map((opportunity, i) => {
+                return {
+                    name: opportunity.label,
+                    time: opportunity.time_commitment,
+                    steps: JSON.stringify(opportunity.steps)
+                }
+            })
+            this.setState({data})       
+            return data
+        } else {
+            return this.state.data
+        }
     }
 
     render() {
         if (this.props.opportunities) {
+            const data = this._loadData()
             return (
-            <table className="opportunitiesList">
-                <thead>
-                    <tr>
-                        <th>Opportunity</th>
-                        <th>Time</th>
-                        <th>Steps</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    this.props.opportunities.map((opportunity, i) => (
-                    <tr key={`opportunity-${i}`} onClick={() => { 
-                        this.props.callbacks.setOpportunity(opportunity)
-                    }}>
-                        <td>{opportunity.label}</td>
-                        <td><span>{opportunity.time_commitment}</span></td>
-                        <td>
-                            <ul>
-                            {
-                                opportunity.steps.map((step, i) => (
-                                    <li key={`opportunity-${i}_step-${i}`}><span>{step.label}</span> {step.description}</li>
-                                ))
-                            }
-                            </ul>
-                        </td>
-
-                    </tr>              
-                    ))
-                }
-                </tbody>
-            </table>)
+                <table>
+                    <thead>
+                    <TableFilter 
+                        rows={data} >
+                        <th filterkey="name">
+                        Name
+                        </th>
+                        <th filterkey="time">
+                        Time
+                        </th>
+                        <th filterkey="steps">
+                        Steps
+                        </th>
+                    </TableFilter>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.opportunities.map((opportunity, i) => {
+                                return (<tr key={`opportunity-${i}`} onClick={() => { 
+                                    this.props.callbacks.setOpportunity(opportunity)
+                                }}>
+                                    <td>{opportunity.label}</td>
+                                    <td><span>{opportunity.time_commitment}</span></td>
+                                    <td>
+                                        <ul>
+                                        {
+                                            opportunity.steps.map((step, i) => (
+                                                <li key={`opportunity_step-${i}`}><span>{step.label}</span> {step.description}</li>
+                                            ))
+                                        }
+                                        </ul>
+                                    </td>
+            
+                                </tr>)  
+                            })
+                        }
+                    </tbody>
+                </table>
+            )
         } else {
             return <div>No data available.</div>
         }
